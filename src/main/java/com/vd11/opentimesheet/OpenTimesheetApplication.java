3e88664d1ee1a167ea1ce69c5006db990168a4c4
beou <hoaivubk@gmail.com>
2018-09-20 08:02:57 +0700

    init commit

---
+++ src/main/java/com/vd11/opentimesheet/OpenTimesheetApplication.java
@@ -0,0 +1,12 @@
+package com.vd11.opentimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenTimesheetApplication.class, args);
+    }
+}
